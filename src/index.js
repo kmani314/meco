1aabb700f7a1945c857e8fee51c4df2de00571d4
Krishna Mani <krishnamani314@gmail.com>
2020-09-13 14:56:27 -0700

    signals

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import m from 'mithril';
+import Qty from 'js-quantities';
 import render from './render';
+import Signal from './signal';
 
 m.mount(document.body, {
   view: () => m(render),
 });
+
+const s1 = new Signal({
+  txPower: Qty(100, 'W'),
+  ple: 1,
+  frequency: Qty(100, 'hz'),
+});
+
+s1.selfTick(Qty(1, 's'));
+
+console.log(s1.intensity);
+
+const v = [Qty(5000 ** 0.5, 'km/s'), Qty(5000 ** 0.5, 'km/s'), Qty(0, 'm/s')];
+const p = [Qty(2 ** 0.5, 'm'), Qty(2 ** 0.5, 'm'), Qty(0, 'm')];
+
+console.log(s1.dopplerShift(v, p));
---
+++ src/signal.js
@@ -0,0 +1,54 @@
+import Qty from 'js-quantities';
+import constants from './constants';
+import util from './util';
+
+export default class Signal {
+  constructor(props) {
+    this.origin = props.origin || [Qty(0, 'm'), Qty(0, 'm'), Qty(0, 'm')];
+
+    // speed of light if propagation value is not provided
+    this.propagation = props.propagation || constants.c;
+
+    // signal has intensity of infinity at the moment of transmission
+    this.intensity = Qty(0, 'W/m^2');
+    this.txPower = props.txPower;
+
+    // Path loss exponent
+    // not used yet (zero attenuation in free space)
+    this.ple = props.ple;
+    this.frequency = props.frequency;
+    this.wavelength = this.propagation.div(this.frequency);
+    this.radius = Qty(0, 'm');
+  }
+
+  selfTick(tick) {
+    // new radius is old radius + speed of light * time
+    this.radius = this.radius.add(this.propagation.mul(tick));
+
+    // inverse square law
+    // this will break if r = 0 (some power over zero area must yield infinite power per area)
+    // note: this is assumes the signal is isotropically radiated signal.
+    const a = Qty(this.radius.scalar ** 2, `${this.radius.units()}^2`).mul(Qty(4 * Math.PI, '1'));
+    this.intensity = this.txPower.div(a);
+  }
+
+  hasIntersected(a) {
+    // has the signal reached some point a?
+    const d = util.distanceScalar(a, this.origin);
+    return d.gt(this.radius);
+  }
+
+  dopplerShift(v, p) {
+    // for some object at p moving with v⃗, what doppler shift does it experience?
+    // this breaks when they are at the same point
+    const dir = util.unitVector(
+      util.sumVecs([this.origin, p.map((x) => Qty(-x.scalar, x.units()))]),
+    );
+
+    // dot product
+    const dot = v.map((c, k) => c.mul(dir[k])).reduce((a, b) => a.add(b));
+
+    // doppler shift assuming source is stationary
+    return ((dot.add(this.propagation)).div(this.propagation)).mul(this.frequency);
+  }
+}
